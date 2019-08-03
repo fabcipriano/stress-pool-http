5f9b9771425aeaca057e88acff6358b2fa2ed243
Fabiano C. de Oliveira <fabianocp@algartelecom.com.br>
2019-08-03 19:12:18 -0300

    performance issue is configurable now

--- src/main/java/br/com/facio/labs/stress/pool/http/MainConfiguration.java
+++ src/main/java/br/com/facio/labs/stress/pool/http/MainConfiguration.java
@@ -2,14 +2,13 @@ package br.com.facio.labs.stress.pool.http;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.params.ConnManagerPNames;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +19,16 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MainConfiguration {
+    private static Logger LOG = LoggerFactory.getLogger(MainConfiguration.class);
 
     @Value("${stresspool.http.connection.timeout}")
     private String connectionTimeout;
 
     @Value("${stresspool.http.read.timeout}")
     private String readTimeout;
+    
+    @Value("${stresspool.http.performance.issue}")
+    private boolean performanceIssue;    
 
     @Bean
     public HttpClient httpClient() {
@@ -45,7 +48,14 @@ public class MainConfiguration {
         schemeRegistry.register(
                 new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
 
-        ClientConnectionManager cm = new PoolingClientConnectionManager(schemeRegistry);
+        PoolingClientConnectionManager cm = new PoolingClientConnectionManager(schemeRegistry);
+        if (!performanceIssue) {
+            LOG.info("Fixed. Performance OK !!!");
+            cm.setDefaultMaxPerRoute(40);
+            cm.setMaxTotal(400);
+        } else {
+            LOG.warn(" -----> Houston We Have a Problem performance problem !!!!!");
+        }
 
         return cm;
     }

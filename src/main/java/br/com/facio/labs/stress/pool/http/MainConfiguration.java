a4edf91244acbbf9972ec7d0c07b0bdbea1cfe41
Fabiano C. de Oliveira <fabianocp@algartelecom.com.br>
2019-08-05 01:21:47 -0300

    JMX HttpClient instrumentation and report

--- src/main/java/br/com/facio/labs/stress/pool/http/MainConfiguration.java
+++ src/main/java/br/com/facio/labs/stress/pool/http/MainConfiguration.java
@@ -1,7 +1,14 @@
 package br.com.facio.labs.stress.pool.http;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.httpclient.HttpClientMetricNameStrategies;
+import com.codahale.metrics.httpclient.InstrumentedHttpClientConnectionManager;
+import com.codahale.metrics.httpclient.InstrumentedHttpClients;
+import com.codahale.metrics.httpclient.InstrumentedHttpRequestExecutor;
+import com.codahale.metrics.jmx.JmxReporter;
 import java.util.concurrent.TimeUnit;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.ConnectionConfig;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
@@ -10,7 +17,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,7 +40,7 @@ public class MainConfiguration {
 
     @Value("${stresspool.http.performance.issue}")
     private boolean performanceIssue;
-    
+
     @Value("${stresspool.http.max.conn.per.route}")
     private int maxConnPerRoute;
 
@@ -43,38 +49,56 @@ public class MainConfiguration {
 
     @Value("${stresspool.http.max.conn.ttl.inseconds}")
     private int maxConnTtlInSeconds;
-    
+
     @Bean
-    public CloseableHttpClient httpClient() {
+    public MetricRegistry metricRegistryJMX() {
+        LOG.info("Creating Metrics with JMX report ...");
+        MetricRegistry registry = new MetricRegistry();
+        JmxReporter reporter = JmxReporter.forRegistry(registry).build();
+        reporter.start();
+        LOG.info("JMX report started.");
+
+        return registry;
+    }
+
+    @Bean
+    public CloseableHttpClient httpClient(MetricRegistry registry) {
         CloseableHttpClient client = null;
         if (!performanceIssue) {
             LOG.info("------> Default HTTP, Fixed. Performance OK !!!");
-            client = newHttpClient();
+            client = newHttpClient(registry);
         } else {
             LOG.warn("------> Houston We Have a Problem performance problem !!!!!");
-            client = oldSchoolHttpClient(client);
+            client = oldSchoolHttpClient();
         }
 
         return client;
     }
 
-    private CloseableHttpClient newHttpClient() throws NumberFormatException {
-        CloseableHttpClient client;
+    private CloseableHttpClient newHttpClient(MetricRegistry registry) throws NumberFormatException {
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(Integer.parseInt(connectionTimeout))
                 .setConnectTimeout(Integer.parseInt(connectionTimeout))
                 .setSocketTimeout(Integer.parseInt(readTimeout)).build();
-        HttpClientBuilder builder = HttpClientBuilder.create()
+        
+        InstrumentedHttpClientConnectionManager connManager = InstrumentedHttpClientConnectionManager
+                .builder(registry).connTTL(maxConnTtlInSeconds).connTTLTimeUnit(TimeUnit.SECONDS).build();
+        connManager.setDefaultMaxPerRoute(maxConnPerRoute);
+        connManager.setMaxTotal(maxConnTotal);
+        connManager.setDefaultConnectionConfig(ConnectionConfig.DEFAULT);
+                
+        CloseableHttpClient client = HttpClientBuilder.create()
                 .setDefaultRequestConfig(requestConfig)
-                .setConnectionTimeToLive(maxConnTtlInSeconds, TimeUnit.SECONDS)
-                .setMaxConnPerRoute(maxConnPerRoute)
-                .setMaxConnTotal(maxConnTotal);
-        client = builder.build();
+                .disableAutomaticRetries()
+                .setRequestExecutor(
+                        new InstrumentedHttpRequestExecutor(registry, HttpClientMetricNameStrategies.HOST_AND_METHOD))
+                .setConnectionManager(connManager).build();
+        
         return client;
     }
 
-    private CloseableHttpClient oldSchoolHttpClient(CloseableHttpClient client) throws NumberFormatException {
-        client = new DefaultHttpClient(createHttpPool());
+    private CloseableHttpClient oldSchoolHttpClient() throws NumberFormatException {
+        CloseableHttpClient client = new DefaultHttpClient(createHttpPool());
         org.apache.http.params.HttpConnectionParams.setSoTimeout(client.getParams(),
                 Integer.parseInt(readTimeout));
         org.apache.http.params.HttpConnectionParams.setConnectionTimeout(
